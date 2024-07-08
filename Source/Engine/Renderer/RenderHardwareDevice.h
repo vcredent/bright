9c5e48292ea6e4557d8fba956a9c2d2de9c34071
2B键盘 <vcredent@foxmail.com>
2024-07-09 02:25:52 +0800

    initialize portable engine porject.

---
+++ Source/Engine/Renderer/RenderHardwareDevice.h
@@ -0,0 +1,42 @@
+/* ************************************************************************ */
+/* RenderHardwareDevice.h                                                   */
+/* ************************************************************************ */
+/*                        This file is part of:                             */
+/*                           PORTABLE ENGINE                                */
+/* ************************************************************************ */
+/*                                                                          */
+/* Copyright (C) 2022 Vcredent All rights reserved.                         */
+/*                                                                          */
+/* Licensed under the Apache License, Version 2.0 (the "License");          */
+/* you may not use this file except in compliance with the License.         */
+/*                                                                          */
+/* You may obtain a copy of the License at                                  */
+/*     http://www.apache.org/licenses/LICENSE-2.0                           */
+/*                                                                          */
+/* Unless required by applicable law or agreed to in writing, software      */
+/* distributed under the License is distributed on an "AS IS" BASIS,        */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, e1ither express or implied */
+/* See the License for the specific language governing permissions and      */
+/* limitations under the License.                                           */
+/*                                                                          */
+/* ************************************************************************ */
+#ifndef RENDER_HARDWARE_DEVICE_H
+#define RENDER_HARDWARE_DEVICE_H
+
+/* Render hardware driver device abstract interface.
+ *   This class is abstract interface for vulkan/dx/gl etc... */
+class RenderHardwareDevice {
+public:
+    static RenderHardwareDevice *InitHardwareDevice(unsigned int width, unsigned int height, RenderHardwareDevice **ppHardwareDevice)
+    {
+        // ....
+        return nullptr;
+    }
+
+    static void DestroyHardwareDevice(RenderHardwareDevice *pHardwareDevice)
+    {
+        // ....
+    }
+};
+
+#endif /* RENDER_HARDWARE_DEVICE_H */
